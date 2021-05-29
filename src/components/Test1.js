232ecb97b1b3e5d53fab5a9720c7aaaab0d11a0c
elliot <66899070+elliotak47@users.noreply.github.com>
2021-05-30 01:50:59 +0530

    Update Test1.js
    update fixes...

--- src/components/Test1.js
+++ src/components/Test1.js
@@ -5,45 +5,73 @@ function Test1({ cen }) {
 
     return (
         <div>
-            {
-                sess.map((xxx, i) => (
-                    <div>
-                        {xxx.min_age_limit === 18 &&
-                            <div key={i} style={{ display: "flex", backgroundColor: xxx.available_capacity_dose1 > 0 ? "yellow" : "black", color: xxx.available_capacity_dose1 > 0 ? "black" : "white", marginBottom: "20px" }}>
+            {window.innerWidth > 500 ?
+                (<div>
+                    {
+                        sess.map((xxx, i) => (
+                            <div key={i} >
+                                {xxx.min_age_limit === 18 &&
+                                    <div style={{ display: "flex", backgroundColor: xxx.available_capacity_dose1 > 0 ? "yellow" : "black", color: xxx.available_capacity_dose1 > 0 ? "black" : "white", marginBottom: "20px" }}>
+                                        <div style={{ width: "200px", marginBottom: "20px", paddingTop: "20px", textAlign: "center" }}>
+                                            {cen.district_name}
+                                        </div>
+                                        <div style={{ width: "200px", marginBottom: "20px", paddingTop: "20px", textAlign: "center" }}>
+                                            {cen.address}
+                                        </div>
+                                        <div style={{ width: "200px", marginBottom: "20px", paddingTop: "20px", textAlign: "center" }}>
+                                            {xxx.date}
+                                        </div>
+                                        <div style={{ width: "200px", marginBottom: "20px", paddingTop: "20px", textAlign: "center" }}>
+                                            TO: {cen.from}  FROM:{cen.to}
+                                        </div>
+                                        <div style={{ width: "200px", marginBottom: "20px", paddingTop: "20px", textAlign: "center" }}>
+                                            {xxx.min_age_limit}
+                                        </div>
+                                        <div style={{ width: "200px", marginBottom: "20px", paddingTop: "20px", textAlign: "center" }}>
+                                            {cen.fee_type}
+                                        </div>
+                                        <div style={{ width: "200px", marginBottom: "20px", paddingTop: "20px", textAlign: "center" }}>
+                                            {xxx.available_capacity_dose1}
+                                        </div>
+                                    </div>
+                                }
 
-                                <div style={{ width: "200px", marginBottom: "20px", paddingTop: "20px", textAlign: "center" }}>
-                                    {cen.block_name}
-                                </div>
-                                <div style={{ width: "200px", marginBottom: "20px", paddingTop: "20px", textAlign: "center" }}>
-                                    {cen.district_name}
-                                </div>
-                                <div style={{ width: "200px", marginBottom: "20px", paddingTop: "20px", textAlign: "center" }}>
-                                    {cen.address}
-                                </div>
-                                <div style={{ width: "200px", marginBottom: "20px", paddingTop: "20px", textAlign: "center" }}>
-                                     {xxx.date}
-                                </div>
-                                <div style={{ width: "200px", marginBottom: "20px", paddingTop: "20px", textAlign: "center" }}>
-                                    TO: {cen.from}  FROM:{cen.to}
-                                </div>
-                                <div style={{ width: "200px", marginBottom: "20px", paddingTop: "20px", textAlign: "center" }}>
-                                    {xxx.min_age_limit}
-                                </div>
-                                <div style={{ width: "200px", marginBottom: "20px", paddingTop: "20px", textAlign: "center" }}>
-                                    {xxx.vaccine}
-                                </div>
-                                <div style={{ width: "200px", marginBottom: "20px", paddingTop: "20px", textAlign: "center" }}>
-                                    {cen.fee_type}
-                                </div>
-                                <div style={{ width: "200px", marginBottom: "20px", paddingTop: "20px", textAlign: "center" }}>
-                                    {xxx.available_capacity_dose1}
-                                </div>
                             </div>
-                        }
+                        ))
+                    }
+                </div>)
+                :
+                (<div>
+                    {
+                        sess.map((xxx, i) => (
+                            <div>
+                                {xxx.min_age_limit === 18 &&
+                                    <div key={i} style={{ display: "flex", backgroundColor: xxx.available_capacity_dose1 > 0 ? "yellow" : "black", color: xxx.available_capacity_dose1 > 0 ? "black" : "white", marginBottom: "20px" }}>
+                                        <div style={{ width: "200px", marginBottom: "20px", paddingTop: "20px", textAlign: "center" }}>
+                                            {cen.district_name}
+                                        </div>
+                                        <div style={{ width: "200px", marginBottom: "20px", paddingTop: "20px", textAlign: "center" }}>
+                                            {cen.address}
+                                        </div>
+                                        <div style={{ width: "200px", marginBottom: "20px", paddingTop: "20px", textAlign: "center" }}>
+                                            {xxx.date}
+                                        </div>
+                                        <div style={{ width: "200px", marginBottom: "20px", paddingTop: "20px", textAlign: "center" }}>
+                                            {xxx.vaccine}
+                                        </div>
+                                        <div style={{ width: "200px", marginBottom: "20px", paddingTop: "20px", textAlign: "center" }}>
+                                            {cen.fee_type}
+                                        </div>
+                                        <div style={{ width: "200px", marginBottom: "20px", paddingTop: "20px", textAlign: "center" }}>
+                                            {xxx.available_capacity_dose1}
+                                        </div>
+                                    </div>
+                                }
 
-                    </div>
-                ))
-            }
+                            </div>
+                        ))
+                    }
+                </div>)}
         </div>
     )
 }
