782f9ef7995b44c4cbfea02b91f60920bbd359c0
aakash <aakash.apptronix@gmail.com>
2021-05-29 23:35:38 +0530

    from lenovo...

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import axios from 'axios';
+import Test1 from './components/Test1.js';
 
 function App() {
+
+
+  const [center, setCenter] = useState([])
+  const [lanter, setLanter] = useState([])
+
+  async function call() {
+    await axios.get('https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByDistrict?district_id=505&date=30-05-2021')
+      .then(res => {
+        setCenter(res.data.centers)
+      })
+    await axios.get('https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByDistrict?district_id=506&date=30-05-2021')
+      .then(res => {
+        setLanter(res.data.centers)
+      })
+
+  }
+
+  useEffect(() => {
+    axios.get('https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByDistrict?district_id=505&date=30-05-2021')
+      .then(res => {
+        setCenter(res.data.centers)
+      })
+    axios.get('https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByDistrict?district_id=506&date=30-05-2021')
+      .then(res => {
+        setLanter(res.data.centers)
+      })
+
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ backgroundColor: "black" }}>
+      <button onClick={() => call()} >Refresh</button>
+
+      {(center.length > 0) && (
+        <div>
+          <div style={{ display: "flex", backgroundColor: "grey", marginBottom: "20px", paddingTop: "20px", color: "white" }}>
+            <div style={{ width: "200px", marginBottom: "20px", textAlign: "center" }}> <b>Block</b> </div>
+            <div style={{ width: "200px", marginBottom: "20px", textAlign: "center" }}> <b>District-Name</b> </div>
+            <div style={{ width: "200px", marginBottom: "20px", textAlign: "center" }}> <b>Address</b> </div>
+            <div style={{ width: "200px", marginBottom: "20px", textAlign: "center" }}> <b>Date</b> </div>
+            <div style={{ width: "200px", marginBottom: "20px", textAlign: "center" }}> <b>Time</b> </div>
+            <div style={{ width: "200px", marginBottom: "20px", textAlign: "center" }}> <b>Age-Limit</b> </div>
+            <div style={{ width: "200px", marginBottom: "20px", textAlign: "center" }}> <b>Vaccine</b> </div>
+            <div style={{ width: "200px", marginBottom: "20px", textAlign: "center" }}> <b>Type</b> </div>
+            <div style={{ width: "200px", marginBottom: "20px", textAlign: "center" }}> <b>Available-capacity</b> </div>
+          </div>
+          <div style={{ backgroundColor: "white", width: "100%", textAlign: "center" }}><b>JAIPUR-1</b></div>
+          <div>
+            {center.map((cen, i) => {
+              return (
+                <Test1 key={i} cen={cen} count={i} />
+              )
+            })
+            }
+          </div>
+          <div style={{ backgroundColor: "white", textAlign: "center" }}><b>JAIPUR-2</b></div>
+          <div>
+            {lanter.map((cen, i) => {
+              return (
+                <Test1 key={i} cen={cen} count={i} />
+              )
+            })
+            }
+          </div>
+        </div>
+      )}
+
+
     </div>
   );
 }
 
 export default App;
+
+
+
---
+++ src/components/Test1.js
@@ -0,0 +1,52 @@
+import React from 'react'
+
+function Test1({ cen }) {
+    let sess = cen.sessions;
+
+    return (
+        <div>
+            {
+                sess.map((xxx, i) => (
+                    <div>
+                        {xxx.min_age_limit === 18 &&
+                            <div key={i} style={{ display: "flex", backgroundColor: xxx.available_capacity_dose1 > 0 ? "yellow" : "black", color: xxx.available_capacity_dose1 > 0 ? "black" : "white", marginBottom: "20px" }}>
+
+                                <div style={{ width: "200px", marginBottom: "20px", paddingTop: "20px", textAlign: "center" }}>
+                                    {cen.block_name}
+                                </div>
+                                <div style={{ width: "200px", marginBottom: "20px", paddingTop: "20px", textAlign: "center" }}>
+                                    {cen.district_name}
+                                </div>
+                                <div style={{ width: "200px", marginBottom: "20px", paddingTop: "20px", textAlign: "center" }}>
+                                    {cen.address}
+                                </div>
+                                <div style={{ width: "200px", marginBottom: "20px", paddingTop: "20px", textAlign: "center" }}>
+                                    DATE: {xxx.date}
+                                </div>
+                                <div style={{ width: "200px", marginBottom: "20px", paddingTop: "20px", textAlign: "center" }}>
+                                    TO: {cen.from}  FROM:{cen.to}
+                                </div>
+                                <div style={{ width: "200px", marginBottom: "20px", paddingTop: "20px", textAlign: "center" }}>
+                                    {xxx.min_age_limit}
+                                </div>
+                                <div style={{ width: "200px", marginBottom: "20px", paddingTop: "20px", textAlign: "center" }}>
+                                    {xxx.vaccine}
+                                </div>
+                                <div style={{ width: "200px", marginBottom: "20px", paddingTop: "20px", textAlign: "center" }}>
+                                    {cen.fee_type}
+                                </div>
+                                <div style={{ width: "200px", marginBottom: "20px", paddingTop: "20px", textAlign: "center" }}>
+                                    {xxx.available_capacity_dose1}
+                                </div>
+                            </div>
+                        }
+
+                    </div>
+                ))
+            }
+        </div>
+    )
+}
+
+export default Test1
+
